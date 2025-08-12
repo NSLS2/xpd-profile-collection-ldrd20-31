2d4dd8275795e3b2ba2e79b3dc9167ea0f8a0c6e
XPD Operator <xf28id2@lob1-xpd-ws1.nsls2.bnl.local>
2025-08-12 00:06:50 -0400

    backup publisher, agent notes

---
+++ scripts/notes/test_script.py
@@ -0,0 +1,31 @@
+# Add a simple plan
+def count_test(detectors, *, num=1, delay=1):
+    yield from count(detectors, num=num, delay=delay)
+
+# Wait for some time to emulate the script with longer execution time
+ttime.sleep(30)
+
+
+'Cs_033_Br_167_ZnCl_100_OAm_000_Tol_1500', 
+'Cs_033_Br_167_ZnCl_100_OAm_010_Tol_1550', 
+'Cs_033_Br_167_ZnCl_100_OAm_020_Tol_1600', 
+'Cs_033_Br_167_ZnCl_100_OAm_030_Tol_1650', 
+'Cs_033_Br_167_ZnCl_100_OAm_040_Tol_1700', 
+'Cs_033_Br_167_ZnCl_140_OAm_000_Tol_1700', 
+'Cs_033_Br_167_ZnCl_130_OAm_010_Tol_1700', 
+'Cs_033_Br_167_ZnCl_120_OAm_020_Tol_1700', 
+'Cs_033_Br_167_ZnCl_110_OAm_030_Tol_1700']
+
+
+
+[
+'Cs_033_Br_167_ZnCl_100_OAm_000_Tol_1500', 
+'Cs_033_Br_167_ZnCl_100_OAm_010_Tol_1550', 
+'Cs_033_Br_167_ZnCl_100_OAm_020_Tol_1600', 
+'Cs_033_Br_167_ZnCl_100_OAm_030_Tol_1650', 
+'Cs_033_Br_167_ZnCl_100_OAm_040_Tol_1700', 
+'Cs_033_Br_167_ZnCl_110_OAm_030_Tol_1700', 
+'Cs_033_Br_167_ZnCl_120_OAm_020_Tol_1700', 
+'Cs_033_Br_167_ZnCl_130_OAm_010_Tol_1700', 
+'Cs_033_Br_167_ZnCl_140_OAm_000_Tol_1700'
+]
